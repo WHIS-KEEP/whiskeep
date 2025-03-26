27aeea4b4ff34182c318be5c96959da7c66fd36e
Kim HyunJin <koujie11@gmail.com>
2025-03-26 13:17:35 +0900

    [S12P21A409-163] feat: Whisky Builder Annotation을 추가하라

--- BackEnd/src/main/java/com/whiskeep/common/entity/TastingComponent.java
+++ BackEnd/src/main/java/com/whiskeep/common/entity/TastingComponent.java
@@ -1,9 +1,11 @@
 package com.whiskeep.common.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
