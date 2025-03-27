28d3e1852a2d1eb488b35dbcda6b6d35af233720
boriaegi <daeunchang@naver.com>
2025-03-28 02:40:01 +0900

    [S12P21A409-144] feat: 기록이 남겨지지 않은 위스키 조회를 위한 repository를 만들어라

--- BackEnd/src/main/java/com/whiskeep/api/whisky/repository/WhiskyRepository.java
+++ BackEnd/src/main/java/com/whiskeep/api/whisky/repository/WhiskyRepository.java
@@ -1,8 +1,11 @@
 package com.whiskeep.api.whisky.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.whiskeep.api.whisky.domain.Whisky;
 
 public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+	List<Whisky> findWhiskiesNotRatedByMember(Long memberId);
 }
