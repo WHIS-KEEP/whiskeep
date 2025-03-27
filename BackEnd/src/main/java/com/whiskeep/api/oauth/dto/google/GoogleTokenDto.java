f5ef12471f0715149af69819f30ccc65e30fb045
unknown <yoonji0624@naver.com>
2025-03-27 17:48:58 +0900

    [S12P21A409-132] fix: DTOã를 record로 수정하라

--- BackEnd/src/main/java/com/whiskeep/api/oauth/dto/google/GoogleTokenDto.java
+++ BackEnd/src/main/java/com/whiskeep/api/oauth/dto/google/GoogleTokenDto.java
@@ -2,26 +2,10 @@ package com.whiskeep.api.oauth.dto.google;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-@Getter
-@ToString
-@NoArgsConstructor
-public class GoogleTokenDto {
-	@JsonProperty("access_token")
-	private String accessToken;
-
-	@JsonProperty("expires_in")
-	private Integer expiresIn;
-
-	@JsonProperty("scope")
-	private String scope;
-
-	@JsonProperty("token_type")
-	private String tokenType;
-
-	@JsonProperty("id_token")
-	private String idToken;
-}
+public record GoogleTokenDto(
+	@JsonProperty("access_token") String accessToken,
+	@JsonProperty("expires_in") Integer expiresIn,
+	@JsonProperty("scope") String scope,
+	@JsonProperty("token_type") String tokenType,
+	@JsonProperty("id_token") String idToken
+) { }
