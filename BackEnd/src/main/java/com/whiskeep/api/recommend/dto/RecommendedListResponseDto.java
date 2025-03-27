0654abe8c34fa92d77be7f2db53d7a82696e4f42
daeun <daeunchang@naver.com>
2025-03-27 18:06:42 +0900

    [S12P21A409-144] feat: Dto를 만들어라

---
+++ BackEnd/src/main/java/com/whiskeep/api/recommend/dto/RecommendedListResponseDto.java
@@ -0,0 +1,15 @@
+package com.whiskeep.api.recommend.dto;
+
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class RecommendedListResponseDto {
+
+	private List<RecommendResponseDto> recommendList;
+}
