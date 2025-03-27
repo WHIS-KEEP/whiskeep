97622b34ed4324329c5cef8b52bcb5ae756be73c
daeun <daeunchang@naver.com>
2025-03-27 18:06:26 +0900

    [S12P21A409-144] feat: 벡터화 전용 클래스를 작성하라

---
+++ BackEnd/src/main/java/com/whiskeep/api/recommend/model/WhiskyVectorizer.java
@@ -0,0 +1,22 @@
+package com.whiskeep.api.recommend.model;
+
+import java.util.Map;
+
+import com.whiskeep.api.whisky.domain.Whisky;
+import com.whiskeep.common.model.TastingProfile;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WhiskyVectorizer {
+
+	public double[] vetorizeWhisky(TastingProfile<Map<String, Double> profile,>) {
+		TastingProfile<Map<String, Double>> nosing = whisky.getNosing().;
+		TastingProfile<Map<String, Double>> tasting = whisky.getTasting();
+		TastingProfile<Map<String, Double>> finish = whisky.getFinish();
+		whisky.getNosing().
+	}
+}
