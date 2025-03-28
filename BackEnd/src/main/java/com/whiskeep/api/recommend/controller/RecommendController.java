f6868c313c30628a1784940caa14d6816f8b3358
daeun <daeunchang@naver.com>
2025-03-28 16:55:16 +0900

    [S12P21A409-144] feat: 임시사용자를 이용하여 Controller를 완성하라

--- BackEnd/src/main/java/com/whiskeep/api/recommend/controller/RecommendController.java
+++ BackEnd/src/main/java/com/whiskeep/api/recommend/controller/RecommendController.java
@@ -1,14 +1,13 @@
 package com.whiskeep.api.recommend.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.whiskeep.api.recommend.dto.RecommendResponseDto;
 import com.whiskeep.api.recommend.dto.RecommendedListResponseDto;
 import com.whiskeep.api.recommend.service.RecommendService;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,9 +18,12 @@ public class RecommendController {
 	private final RecommendService recommendService;
 
 	@GetMapping //추천가져오기
-	public ResponseEntity<RecommendedListResponseDto> recommend() {
+	public ResponseEntity<RecommendedListResponseDto> recommend(Long memberId) {
 
-		RecommendedListResponseDto recommendations = recommendService.recommend();
+		//로그인 된 멤버 갖고오기
+
+		Long tempId = 1L;
+		RecommendedListResponseDto recommendations = recommendService.recommendWhiskies(tempId);
 		return ResponseEntity.ok(recommendations);
 	}
 }
