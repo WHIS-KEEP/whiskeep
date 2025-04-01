d329b004f5ce929d9eeca0d3dee77ac2e17bbd28
daeun <daeunchang@naver.com>
2025-04-01 16:54:31 +0900

    Revert "[S12P21A409-139] fix: checkstyle error를 해결하라"
    This reverts commit 8022e6c5573147c0bcbc85a76ed5d8557db26c2c.

--- BackEnd/src/main/java/com/whiskeep/common/auth/jwt/JwtTokenProvider.java
+++ BackEnd/src/main/java/com/whiskeep/common/auth/jwt/JwtTokenProvider.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 import com.whiskeep.common.exception.ErrorMessage;
 import com.whiskeep.common.exception.UnauthorizedException;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
