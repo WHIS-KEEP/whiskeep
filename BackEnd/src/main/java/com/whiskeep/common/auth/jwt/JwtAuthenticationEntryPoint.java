a39f5ff67d283ab6ff7935c15dba8120b1f6d476
unknown <yoonji0624@naver.com>
2025-04-02 09:06:05 +0900

    [S12P21A409-185] feat: BE Token관련 예외 Handler를 작성하라

--- BackEnd/src/main/java/com/whiskeep/common/auth/jwt/JwtAuthenticationEntryPoint.java
+++ BackEnd/src/main/java/com/whiskeep/common/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException authException) throws IOException, ServletException {
-
-
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 응답
 	}
 }
