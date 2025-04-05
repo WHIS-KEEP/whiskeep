1c3264c6f04d452d9d7d7e245219ba06a0145ed0
Kim <koujie11@naver.com>
2025-04-05 18:25:15 +0900

    [S12P21A409-203] style: code checkstyle을 수정하라

--- BackEnd/src/main/java/com/whiskeep/api/whisky/service/WhiskyService.java
+++ BackEnd/src/main/java/com/whiskeep/api/whisky/service/WhiskyService.java
@@ -29,7 +29,6 @@ import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
-
 import lombok.RequiredArgsConstructor;
 
 @Service
