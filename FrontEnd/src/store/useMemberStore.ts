33bf1acb804b0a31c9a8f0d0432e8788efb12781
Kim <koujie11@naver.com>
2025-04-06 21:35:40 +0900

    [S12P21A409-189] refactor: 사용자 점수를 zustans persist로 sessionStorage에 추가하라

--- FrontEnd/src/store/useMemberStore.ts
+++ FrontEnd/src/store/useMemberStore.ts
@@ -1,15 +1,24 @@
 import { MemberScore } from '@/types/member';
 import { create } from 'zustand';
-// 위에서 정의한 타입
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface UserState {
-  score: MemberScore | null;
+  score: MemberScore | null; // 사용자 점수 상태
   setUserScore: (score: MemberScore) => void;
 }
 
-const useMemberStore = create<UserState>((set) => ({
-  score: null,
-  setUserScore: (score) => set({ score }),
-}));
+// zustand 스토어 생성 및 persist 미들웨어 적용
+const useMemberStore = create<UserState>()(
+  persist(
+    (set) => ({
+      score: null, // 초기값은 null
+      setUserScore: (score) => set({ score }), // 상태 업데이트 함수
+    }),
+    {
+      name: 'member-score', // sessionStorage에 저장될 key 이름
+      storage: createJSONStorage(() => sessionStorage), // sessionStorage에 JSON 형식으로 저장
+    },
+  ),
+);
 
 export default useMemberStore;
