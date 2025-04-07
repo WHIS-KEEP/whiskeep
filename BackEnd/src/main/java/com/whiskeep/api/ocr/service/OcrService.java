f9f77c071b575d6168814288fe4f07a10c23b6aa
unknown <yoonji0624@naver.com>
2025-04-07 16:55:18 +0900

    [S12P21A409-133] fix: BE checkstyle에 맞게 수정하라

--- BackEnd/src/main/java/com/whiskeep/api/ocr/service/OcrService.java
+++ BackEnd/src/main/java/com/whiskeep/api/ocr/service/OcrService.java
@@ -1,5 +1,9 @@
 package com.whiskeep.api.ocr.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,13 +14,12 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class OcrService {
 
-	private final String FASTAPI_OCR_URL = "http://localhost:8000/ocr/";
+	@Value("${ocr.api-url}")
+	private String fastApiOcrUrl;
 	private final RestTemplate restTemplate = new RestTemplate();
 
 	public Map<String, Object> performOcr(MultipartFile file) {
@@ -35,7 +38,7 @@ public class OcrService {
 			HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
 			ResponseEntity<Map> response = restTemplate.exchange(
-				FASTAPI_OCR_URL, HttpMethod.POST, requestEntity, Map.class
+				fastApiOcrUrl, HttpMethod.POST, requestEntity, Map.class
 			);
 
 			return response.getBody();
