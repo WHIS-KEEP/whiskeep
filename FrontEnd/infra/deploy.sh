b92b6f277b1f1a17876755690e4f8273daa72f67
오수영 <ohsusu515@gmail.com>
2025-04-28 09:46:15 +0900

    [S12P21A409-252] refactor: 주석을 제거하라

--- FrontEnd/infra/deploy.sh
+++ FrontEnd/infra/deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# deploy.sh
 
-# 현재 스크립트가 있는 디렉토리(FrontEnd/infra)로 이동
 cd "$(dirname "$0")" || exit
 
-# 기존 컨테이너와 orphan 컨테이너 정리
 docker compose -p frontend -f docker-compose.dev.yml down --remove-orphans
 
-# docker-compose.dev.yml 파일을 이용해 서비스 실행 (백그라운드 실행)
 docker compose -p frontend -f docker-compose.dev.yml up -d --build
 
-# 실행 후 상태 확인 (옵션)
 docker compose -p frontend -f docker-compose.dev.yml ps
